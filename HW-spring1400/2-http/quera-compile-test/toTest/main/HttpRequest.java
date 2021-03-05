08e804f0abd0366f47edbe290e6941b4dcc7c888
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2021-03-05 18:34:38 +0330

    add sample compilable solution

---
+++ HW-spring1400/2-http/quera-compile-test/toTest/main/HttpRequest.java
@@ -0,0 +1,75 @@
+package main;
+
+import java.io.*;
+import java.net.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpRequest {
+    private String url;
+    private HttpRequestMethod httpRequestMethod;
+    private HashMap<String, String> headers;
+    private HashMap<String, String> params;
+    private HashMap<String, String> body;
+
+    public HttpRequest(String url) {
+        this(url, HttpRequestMethod.GET, new HashMap<>(), new HashMap<>(), new HashMap<>());
+    }
+
+    public HttpRequest(String url, HttpRequestMethod httpRequestMethod) {
+        this(url, httpRequestMethod, new HashMap<>(), new HashMap<>(), new HashMap<>());
+    }
+
+    public HttpRequest(String url, HttpRequestMethod httpRequestMethod, HashMap<String, 
+            String> headers, HashMap<String, String> params, HashMap<String, String> body) {
+        this.url = url;
+        this.httpRequestMethod = httpRequestMethod;
+        this.headers = headers;
+        this.params = params;
+        this.body = body;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public HttpRequestMethod getHttpRequestMethod() {
+        return this.httpRequestMethod;
+    }
+
+    public void setHttpRequestMethod(HttpRequestMethod httpRequestMethod) {
+        this.httpRequestMethod = httpRequestMethod;
+    }
+
+    public HashMap<String, String> getHeaders() {
+        return this.headers;
+    }
+
+    public void setHeaders(HashMap<String, String> headers) {
+        this.headers = headers;
+    }
+
+    public HashMap<String, String> getParams() {
+        return this.params;
+    }
+
+    public void setParams(HashMap<String, String> params) {
+        this.params = params;
+    }
+
+    public HashMap<String, String> getBody() {
+        return this.body;
+    }
+
+    public void setBody(HashMap<String, String> body) {
+        this.body = body;
+    }
+
+    public HttpResponse request() throws HttpException, IOException {
+        throw new RuntimeException();
+    }
+}
