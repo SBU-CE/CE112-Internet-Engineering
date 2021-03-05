9f58703acf32171e0314e6506c5e40a34be3daed
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2021-03-05 18:19:07 +0330

    updated tests

--- HW-spring1400/2-http/tests/tests/test/SampleTest.java
+++ HW-spring1400/2-http/tests/tests/test/SampleTest.java
@@ -7,26 +7,75 @@ import org.junit.*;
 
 import static org.junit.Assert.*;
 
+import java.util.*;
+
 import java.io.IOException;
 
 public class SampleTest {
-    private HttpRequest httpRequest;
     final static String url = "http://httpbin.org/anything";
 
-    @Before
-    public void setUp() {
-        httpRequest = new HttpRequest(url);
+    @Test
+    public void oneParamConsTest() {
+        HttpRequest httpRequest = new HttpRequest(url);
+        assertEquals(HttpRequestMethod.GET, httpRequest.getHttpRequestMethod());
     }
 
     @Test
-    public void createHttpRequestWithUrlAndMethod() {
-        assertEquals(url, this.httpRequest.getUrl());
-        assertEquals(HttpRequestMethod.GET, this.httpRequest.getHttpRequestMethod());
+    public void twoParamConsTest() {
+        HttpRequest httpRequest = new HttpRequest(url, HttpRequestMethod.POST);
     }
 
     @Test
-    public void successfulGetRequest() throws Exception{
-            HttpResponse httpResponse = this.httpRequest.request();
-            assertEquals(200, httpResponse.getStatus());
+    public void allConsTest() {
+        HttpRequest httpRequest =
+            new HttpRequest(url, HttpRequestMethod.POST,
+                            new HashMap<String, String>(),
+                            new HashMap<String, String>(),
+                            new HashMap<String, String>()
+                           );
     }
+
+    @Test
+    public void urlTest() {
+        String actual = new HttpRequest(url).getUrl();
+        assertEquals(url, actual);
+    }
+
+    @Test
+    public void methodTwoParamTest() {
+        HttpRequest httpRequest = new HttpRequest(url, HttpRequestMethod.POST);
+        assertEquals(HttpRequestMethod.POST, httpRequest.getHttpRequestMethod());
+    }
+
+    @Test
+    public void settersTest() {
+        HttpRequest hr = new HttpRequest(url, HttpRequestMethod.POST);
+        hr.setUrl("https://google.com");
+        hr.setParams(new HashMap<String, String>());
+        hr.setHeaders(new HashMap<String, String>());
+        hr.setBody(new HashMap<String, String>());
+        hr.setHttpRequestMethod(HttpRequestMethod.PUT);
+    }
+
+    @Test
+    public void gettersTest() {
+        HttpRequest hr = new HttpRequest(url, HttpRequestMethod.POST);
+        String url = hr.getUrl();
+        HashMap<String, String> hm = hr.getParams();
+        hm = hr.getHeaders();
+        hm = hr.getBody();
+        HttpRequestMethod method = hr.getHttpRequestMethod();
+    }
+
+    @Test
+    public void requestTest() {
+        try {
+            HttpRequest hr = new HttpRequest(url, HttpRequestMethod.POST);
+            HttpResponse httpResponse = hr.request();
+            //assertFalse(200 == httpResponse.getStatus());
+        } catch (IOException | HttpException e) {
+            // thats ok
+        }
+    }
+
 }
