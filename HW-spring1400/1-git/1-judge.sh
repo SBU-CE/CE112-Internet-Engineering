8a0824d70a9105f5cdf7224b8362ebd7b8b46dd7
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2021-06-12 15:46:16 +0430

    judge for git1

---
+++ HW-spring1400/1-git/1-judge.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+set -e 
+
+
+mkdir cleaned || true
+
+for folder in ./9* ; do
+    clear
+
+    echo "--> person $folder"
+    cd "$folder"
+    unzip -qu ./*.zip || true
+    cd git-hw
+
+    thunar .
+
+    echo "--> branches :"
+    git branch --list | cat
+
+
+    echo "--> log :"
+    git log --graph --oneline --decorate --abbrev-commit | cat
+
+    echo "--> status :"
+    git status | cat
+
+    echo "--> diff"
+    git diff HEAD | cat
+
+    echo "--> file:"
+    cat homework.txt
+
+
+    echo "\n--> .gitignore :"
+    cat .gitignore || echo "**no gitignore**"
+    echo ""
+
+
+    #bash 
+    read -n1 -r -p "Press any key to next..." key
+
+    cd ..
+    rm result.txt || true
+    rm -- *.zip
+
+    cd ..
+    mv "$folder" cleaned
+done
+
+
